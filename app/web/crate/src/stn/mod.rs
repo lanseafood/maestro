91878bdbe6ee93640fc3eeaf68752f49a683b5e3
Cameron Pittman <cameron.w.pittman@gmail.com>
2020-01-18 13:26:58 -0500

    Add working APSP with tests

--- app/web/crate/src/stn/mod.rs
+++ app/web/crate/src/stn/mod.rs
@@ -1,7 +1,6 @@
 use petgraph::graph::NodeIndex;
 use petgraph::Graph;
 use std::collections::HashMap;
-use std::default::Default;
 use std::string::String;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
@@ -13,6 +12,10 @@ pub use interval::Interval;
 fn default_execution_uncertainty() -> f64 {
   0.1
 }
+
+// fn print_constraint_table(ct: &HashMap<(i32, i32), f64>) {
+//   // TOOD
+// }
 
 #[wasm_bindgen]
 #[derive(Debug, Default, Deserialize, Serialize)]
@@ -68,6 +71,8 @@ fn build_distance_graph(
   for node in nodes.iter() {
     let node_index = stn.distance_graph.add_node(*node);
     stn.node_indices.insert(*node, node_index);
+    // edge from any node to itself needs to be 0
+    stn.distance_graph.add_edge(node_index, node_index, 0.);
   }
 
   // now set the edges with weights
@@ -92,17 +97,85 @@ fn build_distance_graph(
       upper = edge.minutes + error_estimate;
     }
 
-    // outgoing upper interval
-    stn.distance_graph.update_edge(*source, *target, upper);
-
     // incoming negative lower interval
     stn.distance_graph.update_edge(*target, *source, -lower);
+
+    // outgoing upper interval
+    stn.distance_graph.update_edge(*source, *target, upper);
   }
 
   Ok((
     stn.distance_graph.node_count(),
     stn.distance_graph.edge_count(),
   ))
+}
+
+/// Perform All Pairs Shortest Paths algorithm and generate the constraint table
+fn perform_apsp(stn: &mut STN) -> Result<(), String> {
+  // FYI, we don't need to init. distances from a node to a node (i, i) to 0 because we already set (i, i) edge weights to 0 when we created the graph. The 0s will be added in the loop below
+
+  // add known distances to the table
+  for (i, i_node) in stn.node_indices.iter() {
+    for (j, j_node) in stn.node_indices.iter() {
+      // get the edge index if it exists. if not, this isn't a known distance
+      let edge_index = match stn.distance_graph.find_edge(*i_node, *j_node) {
+        Some(e) => e,
+        None => continue,
+      };
+
+      let distance = match stn.distance_graph.edge_weight(edge_index) {
+        Some(d) => d,
+        None => return Err(format!("missing edge weight: [{}, {}]", i, j)),
+      };
+
+      // constraint_table is in (from row, to column) format
+      let position = (*i, *j);
+      stn.constraint_table.insert(position, *distance);
+    }
+  }
+
+  let iter = 1_i32..stn.node_indices.len() as i32 + 1;
+
+  // iterate over intermediates
+  for k in iter.clone() {
+    // i is the row in the constraint table
+    for i in iter.clone() {
+      // j is the column in the constraint table
+      for j in iter.clone() {
+        // constraint_table is in (from row, to column) format
+        let position = (i, j);
+        let d_ik = match stn.constraint_table.get(&(i, k)) {
+          Some(d) => d,
+          None => &std::f64::MAX,
+        };
+        let d_kj = match stn.constraint_table.get(&(k, j)) {
+          Some(d) => d,
+          None => &std::f64::MAX,
+        };
+
+        let d_current = {
+          match stn.constraint_table.get(&position) {
+            Some(d) => d,
+            None => &std::f64::MAX,
+          }
+        };
+
+        let d_new = d_current.min(*d_ik + *d_kj);
+
+        if i == j && d_new < 0. {
+          let error_message = format!(
+            "negative cycle found on node ID {}: {} + {} = {}",
+            i, d_ik, d_kj, d_new
+          );
+          return Err(error_message);
+        }
+
+        stn.constraint_table.insert(position, d_new);
+      }
+    }
+  }
+
+  Ok(())
 }
 
 /// (node count, edge count) tuple struct
@@ -126,7 +199,7 @@ impl STN {
     format!("{} elapsed time", self.elapsed_time)
   }
 
-  /// Register task data as a distance graph. Returns (node count, edge count) tuple
+  /// Register task data as a distance graph. Returns (node count, edge count) tuple. Note that an edge with weight 0 will be created for every node to itself
   #[wasm_bindgen(catch, method, js_name = registerGraph)]
   pub fn register_graph(
     &mut self,
@@ -145,88 +218,10 @@ impl STN {
   /// Perform All Pairs Shortest Paths (Floyd-Warshall) algorithm to calculate inferred constraints.
   #[wasm_bindgen(catch, method, js_name = performAPSP)]
   pub fn perform_apsp(&mut self) -> Result<(), JsValue> {
-    let node_iter = self.node_indices.iter();
-
-    // init. distances from a node to a node to 0
-    for (i, _i_node) in node_iter.clone() {
-      self.constraint_table.insert((*i, *i), 0.);
-    }
-
-    // add known distances to the table
-    for (i, i_node) in node_iter.clone() {
-      for (j, j_node) in node_iter.clone() {
-        // look up the edge index
-        let edge_index = match self.distance_graph.find_edge(*i_node, *j_node) {
-          Some(e) => e,
-          None => {
-            return Err(JsValue::from_str(&format!(
-              "Could not find edge [{}, {}]",
-              i, j,
-            )))
-          }
-        };
-
-        let distance = match self.distance_graph.edge_weight(edge_index) {
-          Some(d) => d,
-          None => {
-            return Err(JsValue::from_str(&format!(
-              "Could not find edge weight [{}, {}]",
-              i, j,
-            )))
-          }
-        };
-
-        self.constraint_table.insert((*i, *j), *distance);
-      }
-    }
-
-    // iterate over intermediates
-    for (_k, k_node) in node_iter.clone() {
-      for (i, i_node) in node_iter.clone() {
-        for (j, j_node) in node_iter.clone() {
-          let from = match self.distance_graph.find_edge(*i_node, *k_node) {
-            Some(e) => e,
-            // if no edge exists, move on
-            None => continue,
-          };
-          let to = match self.distance_graph.find_edge(*k_node, *j_node) {
-            Some(e) => e,
-            // if no edge exists, move on
-            None => continue,
-          };
-
-          let distance_from = match self.distance_graph.edge_weight(from) {
-            Some(d) => d,
-            // if no distance exists, move one
-            None => continue,
-          };
-          let distance_to = match self.distance_graph.edge_weight(to) {
-            Some(d) => d,
-            // if no distance exists, move one
-            None => continue,
-          };
-
-          let current_distance = {
-            match self.constraint_table.get(&(*i, *j)) {
-              Some(d) => d,
-              None => &std::f64::MAX,
-            }
-          };
-
-          let new_distance = current_distance.min(distance_from + distance_to);
-
-          if i == j && new_distance < 0. {
-            let error_message = format!("negative cycle found on node ID {}", i);
-            return Err(JsValue::from_str(&error_message));
-          }
-
-          if new_distance < *current_distance {
-            self.constraint_table.insert((*i, *j), new_distance);
-          }
-        }
-      }
-    }
-    Ok(())
+    match perform_apsp(self) {
+      Ok(()) => return Ok(()),
+      Err(e) => return Err(JsValue::from_str(&e)),
+    };
   }
 }
 
@@ -238,7 +233,7 @@ mod tests {
   use wasm_bindgen_test::*;
 
   #[test]
-  fn test_build_distance_graph() -> Result<(), String> {
+  fn test_build_distance_graph_empty_input() -> Result<(), String> {
     let payload = RegistrationPayload { edges: vec![] };
     let options = RegistrationOptions {
       implicit_intervals: true,
@@ -305,7 +300,7 @@ mod tests {
 
     // just check that the graph was built
     assert_eq!(5_usize, nodes_created, "correct number of nodes created");
-    assert_eq!(10_usize, edges_created, "correct number of edges created");
+    assert_eq!(15_usize, edges_created, "correct number of edges created");
 
     // now make sure edge weights are correct
     struct Case {
@@ -454,7 +449,7 @@ mod tests {
 
     // just check that the graph was built
     assert_eq!(5_usize, nodes_created, "correct number of nodes created");
-    assert_eq!(10_usize, edges_created, "correct number of edges created");
+    assert_eq!(15_usize, edges_created, "correct number of edges created");
 
     // now make sure edge weights are correct
     struct Case {
@@ -553,6 +548,29 @@ mod tests {
       );
     }
 
+    // make sure (i, i) weights are 0
+    for i in 1..6 {
+      let node_index = {
+        match stn.node_indices.get(&i) {
+          Some(n) => n,
+          None => panic!("could not find index {}", i),
+        }
+      };
+      let edge = {
+        match stn.distance_graph.find_edge(*node_index, *node_index) {
+          Some(edge) => edge,
+          None => panic!("could not find edge indices ({} - {})", i, i),
+        }
+      };
+      let weight = {
+        match stn.distance_graph.edge_weight(edge) {
+          Some(w) => w,
+          None => panic!("could not find weight between indices ({} - {})", i, i),
+        }
+      };
+      assert_eq!(0., *weight, "({} - {}) = 0 got {}", i, i, *weight);
+    }
+
     Ok(())
   }
 
@@ -594,7 +612,6 @@ mod tests {
   fn test_register_graph_converts_json_two_nodes_two_edges() {
     let input = json!(
       {
-        "nodes": [{"id": 0, "label": "X_0"}, {"id": 1, "label": "Ls"}],
         "edges": [{"minutes": 60, "source": 0, "target": 1}],
       }
     );
@@ -619,11 +636,108 @@ mod tests {
 
     match stn.register_graph(&payload, &options) {
       Ok(u) => assert_eq!(
-        (2_usize, 2_usize),
+        (2_usize, 4_usize),
         (u.0, u.1),
-        "Two nodes, two edges expected to be made"
+        "Two nodes, 4 edges expected to be made"
       ),
       Err(e) => panic!("failed running stn.register_graph | {:?}", e),
     }
+  }
+
+  #[test]
+  fn test_perform_apsp_against_walkthrough_data() -> Result<(), String> {
+    // define the graph from the walkthrough
+    let edges = vec![
+      Edge {
+        source: 1,
+        target: 2,
+        interval: Interval::new(10., 20.),
+        minutes: 0.,
+      },
+      Edge {
+        source: 2,
+        target: 3,
+        interval: Interval::new(30., 40.),
+        minutes: 0.,
+      },
+      Edge {
+        source: 4,
+        target: 3,
+        interval: Interval::new(10., 20.),
+        minutes: 0.,
+      },
+      Edge {
+        source: 4,
+        target: 5,
+        interval: Interval::new(40., 50.),
+        minutes: 0.,
+      },
+      Edge {
+        source: 1,
+        target: 5,
+        interval: Interval::new(60., 70.),
+        minutes: 0.,
+      },
+    ];
+
+    let data = RegistrationPayload { edges: edges };
+
+    let options = RegistrationOptions {
+      implicit_intervals: false,
+      execution_uncertainty: 0.,
+    };
+
+    let mut stn = STN::new();
+    build_distance_graph(&mut stn, &data, &options)?;
+    perform_apsp(&mut stn)?;
+
+    // full STN with implicit constraints from the walkthrough example
+    // ((from, to), distance)
+    let expected_constraint_table: HashMap<(i32, i32), f64> = [
+      ((1, 1), 0.),
+      ((1, 2), 20.),
+      ((1, 3), 50.),
+      ((1, 4), 30.),
+      ((1, 5), 70.),
+      ((2, 1), -10.),
+      ((2, 2), 0.),
+      ((2, 3), 40.),
+      ((2, 4), 20.),
+      ((2, 5), 60.),
+      ((3, 1), -40.),
+      ((3, 2), -30.),
+      ((3, 3), 0.),
+      ((3, 4), -10.),
+      ((3, 5), 30.),
+      ((4, 1), -20.),
+      ((4, 2), -10.),
+      ((4, 3), 20.),
+      ((4, 4), 0.),
+      ((4, 5), 50.),
+      ((5, 1), -60.),
+      ((5, 2), -50.),
+      ((5, 3), -20.),
+      ((5, 4), -40.),
+      ((5, 5), 0.),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    assert_eq!(
+      expected_constraint_table.len(),
+      stn.constraint_table.len(),
+      "constraint tables are the same size"
+    );
+
+    for (i, dist) in expected_constraint_table.iter() {
+      assert_eq!(
+        *dist, stn.constraint_table[i],
+        "{:?} want {}, got {}",
+        i, *dist, stn.constraint_table[i],
+      )
+    }
+
+    Ok(())
   }
 }
